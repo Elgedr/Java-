b6d22c3ac51a3da4d866fee796a401f3a9c7cf6c
elgedr <elgedr@ttu.ee>
2021-01-27 20:14:32 +0200

    1

---
+++ PR/PR01Introduction/src/ee/taltech/iti0202/introduction/Introduction.java
@@ -0,0 +1,71 @@
+package ee.taltech.iti0202.introduction;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Introduction {
+
+
+    /**
+     * Method gets two numbers as parameters.
+     * Method must answer if the given pair gives bad, normal or good outcome.
+     * Outcome is "bad" if any of values is less than 5.
+     * Outcome is "good" if one value equals doubled second value.
+     * Outcome is "ok" if both values equal at least 5.
+     * The priority is as follows: "bad", "good", "ok" (if several cases apply, take the higher / earlier).
+     *
+     * @param valueOne int
+     * @param valueTwo int
+     * @return String based on the values of valueOne and valueTwo
+     */
+    public String howIsOutcome(int valueOne, int valueTwo) {
+        if ((valueOne < 5) || (valueTwo < 5)){
+            return "bad";
+        } else if (((valueOne * 2) == valueTwo) || ((valueTwo * 2) == valueOne)){
+            return "good";
+        } else {
+            return "ok";
+        }
+    }
+
+    /**
+     * Method gets a list of numbers.
+     * Return a list containing only even numbers of the given list.
+     * If the given list does not contain any even numbers, return an empty list.
+     *
+     * @param numbers given list that contains numbers.
+     * @return list of even numbers.
+     */
+    public List<Integer> findEvenNumbersList(List<Integer> numbers) {
+        return null;
+    }
+
+    /**
+     * Method gets an array of numbers.
+     * Return an array containing only even numbers of the given array.
+     * If the given array does not contain any even numbers, return an empty array.
+     *
+     * You must not use the previous function in this function!
+     *
+     * @param numbers given array that contains numbers.
+     * @return array of even numbers.
+     */
+    public int[] findEvenNumbersArray(int[] numbers) {
+        return null;
+    }
+
+    /**
+     * Run tests.
+     * @param args info.
+     */
+    public static void main(String[] args) {
+        Introduction introduction = new Introduction();
+        System.out.println(introduction.howIsOutcome(6, 12)); // "bad"
+
+        List<Integer> nums = new ArrayList<>(Arrays.asList(4, 7, 5, 2, 1, 2, -2, 0));
+        System.out.println(introduction.findEvenNumbersList(nums)); // [4, 2, 2, -2, 0]
+
+        int[] array = {9, 0, 24, -6, 3};
+        System.out.println(Arrays.toString(introduction.findEvenNumbersArray(array))); // [0, 24, -6]
+    }
+}
