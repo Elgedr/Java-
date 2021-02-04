3bf15731dead7954491ce5df7dc1761af943b046
elgedr <elgedr@ttu.ee>
2021-02-04 19:34:55 +0200

    1

--- EX/EX02WebBrowser/src/ee/taltech/iti0202/webbrowser/WebBrowser.java
+++ EX/EX02WebBrowser/src/ee/taltech/iti0202/webbrowser/WebBrowser.java
@@ -1,28 +1,50 @@
 package ee.taltech.iti0202.webbrowser;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class WebBrowser {
+    private String startPage = "google.com";
     private String homePage;
+    private String currentPage = "google.com";
+    private String lastPage;
+    private String forwarded;
+
+    private List<String> bookMarkedSites = new ArrayList<>();
+    private List<String> visitedSites = Arrays.asList("google.com");
+    private Map<String, Integer> visitings = new HashMap<>();
+
+    private Boolean back = false;
 
     /**
      * Goes to homepage.
      */
     public void homePage() {
-        //TODO: implement
+        currentPage = homePage;
+        if (!currentPage.equals(lastPage)){
+            visitedSites.add(homePage);
+        }
     }
 
     /**
      * Goes back to previous page.
      */
     public void back() {
-        //TODO: implement
+        back = true;
+
     }
 
     /**
      * Goes forward to next page.
      */
     public void forward() {
-        //TODO: implement
+        if (back){
+            visitedSites.add(forwarded);
+            currentPage = forwarded;
+            back = false;
+        }
     }
 
     /**
@@ -31,14 +53,18 @@ public class WebBrowser {
      * @param url url to go to
      */
     public void goTo(String url) {
-        //TODO: implement
+        if (!currentPage.equals(url)){
+            lastPage = currentPage;
+            currentPage = url;
+            visitedSites.add(url);
+        }
     }
 
     /**
      * Add a webpage as a bookmark.
      */
     public void addAsBookmark() {
-        //TODO: implement
+        bookMarkedSites.add(currentPage);
     }
 
     /**
@@ -47,18 +73,24 @@ public class WebBrowser {
      * @param bookmark to remove
      */
     public void removeBookmark(String bookmark) {
-        //TODO: implement
+        bookMarkedSites.remove(bookmark);
     }
 
+    /**
+     *
+     * @return List of book marked sites.
+     */
     public List<String> getBookmarks() {
-        //TODO: implement
-        return null;
+        return bookMarkedSites;
     }
 
+    /**
+     *
+     * @param homePage homepage name to set.
+     */
     public void setHomePage(String homePage) {
-        //TODO: implement
+        this.homePage = homePage;
     }
-
 
     /**
      * Get top 3 visited pages.
@@ -80,8 +112,7 @@ public class WebBrowser {
      * @return list of all visited pages
      */
     public List<String> getHistory() {
-        //TODO: implement
-        return null;
+        return visitedSites;
     }
 
 
@@ -91,7 +122,6 @@ public class WebBrowser {
      * @return active web page
      */
     public String getCurrentUrl() {
-        //TODO: implement
-        return null;
+        return currentPage;
     }
 }
