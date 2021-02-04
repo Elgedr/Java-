8ad23985c96b7d35fc4d449b6d7c336f89056c08
elgedr <elgedr@ttu.ee>
2021-02-04 22:56:46 +0200

    1

--- EX/EX02WebBrowser/src/ee/taltech/iti0202/webbrowser/WebBrowserTest.java
+++ EX/EX02WebBrowser/src/ee/taltech/iti0202/webbrowser/WebBrowserTest.java
@@ -5,48 +5,67 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class WebBrowserTest {
-
+    /**Test1*/
     @org.junit.Test
     public void homePage() {
     }
 
+
+    /**Test2*/
     @org.junit.Test
     public void back() {
 
-    }
 
+    }
+    /**Test3*/
     @org.junit.Test
     public void forward() {
     }
 
+
+    /**Test4*/
     @org.junit.Test
     public void goTo() {
     }
 
+
+    /**Test5*/
     @org.junit.Test
     public void addAsBookmark() {
     }
 
+
+    /**Test6*/
     @org.junit.Test
     public void removeBookmark() {
     }
 
+
+    /**Test7*/
     @org.junit.Test
     public void getBookmarks() {
     }
 
+
+    /**Test8*/
     @org.junit.Test
     public void setHomePage() {
     }
 
+
+    /**Test9*/
     @org.junit.Test
     public void getTop3VisitedPages() {
     }
 
+
+    /**Test10*/
     @org.junit.Test
     public void getHistory() {
     }
 
+
+    /**Test11*/
     @org.junit.Test
     public void getCurrentUrl() {
     }
