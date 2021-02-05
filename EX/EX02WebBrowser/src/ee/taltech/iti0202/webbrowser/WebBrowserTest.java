172b86310102d97166a2089e4003183e36ad3254
elgedr <elgedr@ttu.ee>
2021-02-05 19:47:56 +0200

    1

--- EX/EX02WebBrowser/src/ee/taltech/iti0202/webbrowser/WebBrowserTest.java
+++ EX/EX02WebBrowser/src/ee/taltech/iti0202/webbrowser/WebBrowserTest.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.*;
 
 public class WebBrowserTest {
     public static final int THIRTY = 30;
