4472a8f6d354d9471c0d990793cd4b7620e70400
elgedr <elgedr@ttu.ee>
2021-02-09 17:10:34 +0200

    1

--- PR/PR03Ring/src/ee/taltech/iti0202/lotr/Person.java
+++ PR/PR03Ring/src/ee/taltech/iti0202/lotr/Person.java
@@ -1,8 +1,8 @@
 package ee.taltech.iti0202.lotr;
 
 public class Person {
-    private String race;
-    private String name;
+    private final String race;
+    private final String name;
     private Ring ring;
 
     public Person(String race, String name, Ring ring) {
@@ -33,16 +33,50 @@ public class Person {
     }
 
     public String isSauron() {
-        if (getName().equals("Sauron") || getRing().getType().equals(Ring.Type.THE_ONE) || getRing().getMaterial() == Ring.Material.GOLD){
+        if (getName().equals("Sauron") && getRing().getType().equals(Ring.Type.THE_ONE) && getRing().getMaterial() == Ring.Material.GOLD){
             return "Affirmative";
-        } else if (getName().equals("Sauron") || getRing().getType().equals(Ring.Type.THE_ONE) || !(getRing().getMaterial() == Ring.Material.GOLD)){
+        } else if (getName().equals("Sauron") && getRing().getType().equals(Ring.Type.THE_ONE) && !(getRing().getMaterial() == Ring.Material.GOLD)){
             return "No, the ring is fake!";
-        } else if (!(getName().equals("Sauron")) || getRing().getType().equals(Ring.Type.THE_ONE) || getRing().getMaterial() == Ring.Material.GOLD){
+        } else if (!(getName().equals("Sauron")) && getRing().getType().equals(Ring.Type.THE_ONE) && getRing().getMaterial() == Ring.Material.GOLD){
             return "No, he just stole the ring.";
-        } else if (getName().equals("Sauron") || !(getRing().getType().equals(Ring.Type.THE_ONE)) || getRing().getMaterial() == Ring.Material.GOLD){
-            return "No, he just stole the ring.";
+        } else if (getName().equals("Sauron") && !(getRing().getType().equals(Ring.Type.THE_ONE)) && getRing().getMaterial() == Ring.Material.GOLD){
+            return " No, but he's claiming to be.";
         } else {
             return "No";
         }
+    }
+
+    public static void main(String[] args) {
+        // LOTR simplified play through
+        Ring theRing = new Ring(Ring.Type.THE_ONE, Ring.Material.GOLD);
+        Person sauron = new Person("Maiar", "Sauron");
+        sauron.setRing(theRing);
+// after some 4000 years, Gollum got the ring
+        Person gollum = new Person("Hobbit", "Gollum");
+// let's remove ring from Sauron
+        sauron.setRing(null);
+        gollum.setRing(theRing);
+// after about 500 years, Bilbo got the ring
+        Person bilbo = new Person("Hobbit", "Bilbo Baggins");
+        gollum.setRing(null);
+        bilbo.setRing(theRing);
+// after 60 years, Frodo got the ring
+        Person frodo = new Person("Hobbit", "Frodo Baggins");
+        bilbo.setRing(null);
+        frodo.setRing(theRing);
+
+// check Sauron
+        Ring fakeOne = new Ring(Ring.Type.THE_ONE, Ring.Material.PLASTIC);
+        sauron.setRing(fakeOne);
+        System.out.println(fakeOne.getMaterial());
+        System.out.println(sauron.isSauron()); // No, the ring is fake!
+        System.out.println(frodo.isSauron()); // No, he just stole the ring
+        Ring nenya = new Ring(Ring.Type.NENYA, Ring.Material.DIAMOND);
+        sauron.setRing(nenya);
+        System.out.println(sauron.isSauron()); // No, but he's claiming to be
+        frodo.setRing(nenya);
+        System.out.println(frodo.isSauron()); // No
+        sauron.setRing(theRing);
+        System.out.println(sauron.isSauron()); // Affirmative
     }
 }
