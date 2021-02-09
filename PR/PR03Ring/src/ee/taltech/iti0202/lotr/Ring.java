d63708831dc27d3669433994512aac6f130b8e62
elgedr <elgedr@ttu.ee>
2021-02-09 23:08:21 +0200

    1

--- PR/PR03Ring/src/ee/taltech/iti0202/lotr/Ring.java
+++ PR/PR03Ring/src/ee/taltech/iti0202/lotr/Ring.java
@@ -8,15 +8,28 @@ public class Ring {
     enum Type { THE_ONE, GOLDEN, NENYA, OTHER }
     enum Material { GOLD, SILVER, BRONZE, PLASTIC, DIAMOND }
 
+    /**
+     *
+     * @param type type of ring from enum
+     * @param material material of ring from enum
+     */
     public Ring(Type type, Material material) {
         this.type = type;
         this.material = material;
     }
 
+    /**
+     *
+     * @return type of ring
+     */
     public Type getType() {
         return this.type;
     }
 
+    /**
+     *
+     * @return material of ring
+     */
     public Material getMaterial() {
         return this.material;
     }
