c49b258ca55f88f28615f958d4d36b9fb0272094
elgedr <elgedr@ttu.ee>
2021-02-11 00:32:39 +0200

    1

--- EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Book.java
+++ EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Book.java
@@ -11,12 +11,19 @@ public class Book {
     private int id = -1; //ид должен начинаться с 0. так как при создании делаем +1, как раз первый ид будет 0
     private Person owner;
 
+    /**
+     *
+     * @param title book's title
+     * @param author book's author
+     * @param yearOfPublishing book's year of publishing
+     * @param price book's price
+     */
     public Book(String title, String author, int yearOfPublishing, int price) {
         this.title = title;
         this.author = author;
         this.yearOfPublishing = yearOfPublishing;
         this.price = price;
-        this.id ++; //при создании новой книги у нее будет новый ид. на 1 больше
+        this.id++; //при создании новой книги у нее будет новый ид. на 1 больше
         futureid = this.id + 1;
     }
 
@@ -81,7 +88,7 @@ public class Book {
      * @param buyer buyer object
      * @return if book was bought or not
      */
-    public boolean buy(Person buyer){
+    public boolean buy(Person buyer) {
         if (this.owner == buyer || buyer.getMoney() < this.price || !buyer.buyBook(this)
                 || !this.owner.sellBook(this)) {
             return false;
