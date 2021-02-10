78872a7e118253aa69320cf7590e0c825b8d57a7
elgedr <elgedr@ttu.ee>
2021-02-10 23:51:29 +0200

    1

--- EX/EX03Bookshelf/src/ee/taltech/iti0202/webbrowser/Book.java
+++ EX/EX03Bookshelf/src/ee/taltech/iti0202/webbrowser/Book.java
@@ -1,4 +1,64 @@
 package ee.taltech.iti0202.webbrowser;
 
 public class Book {
+    private static int futureid;
+    private String title;
+    private String author;
+    private int yearOfPublishing;
+    private int price;
+    private int id = -1; //ид должен начинаться с 0. так как при создании делаем +1, как раз первый ид будет 0
+    private Person owner;
+
+    public Book(String title, String author, int yearOfPublishing, int price){
+        this.title = title;
+        this.author = author;
+        this.yearOfPublishing = yearOfPublishing;
+        this.price = price;
+        this.id ++; //при создании новой книги у нее будет новый ид. на 1 больше
+        futureid = this.id + 1;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getAuthor(){
+        return this.author;
+    }
+
+    public int getYearOfPublishing(){
+        return this.yearOfPublishing;
+    }
+
+    public int getPrice(){
+        return this.price;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+
+    public Person getOwner(){
+        return this.owner;
+    }
+
+    public boolean buy(Person buyer){
+        if (this.owner == buyer || buyer.getMoney() < this.price || !buyer.buyBook(this)
+                || !this.owner.sellBook(this)){
+            return false;
+        } else {
+            this.owner.sellBook(this);
+            buyer.buyBook(this);
+            return true;
+        }
+    }
+
+    public static int getAndIncrementNextId(){
+        return futureid;
+    }
+
 }
--- EX/EX03Bookshelf/src/ee/taltech/iti0202/webbrowser/Person.java
+++ EX/EX03Bookshelf/src/ee/taltech/iti0202/webbrowser/Person.java
@@ -1,4 +1,42 @@
 package ee.taltech.iti0202.webbrowser;
 
 public class Person {
+    private String name;
+    private int money;
+    private Book hisBook;
+
+    public Person(String name, int money){
+        this.name = name;
+        this.money = money;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getMoney(){
+        return this.money;
+    }
+
+    public boolean buyBook(Book book){
+        if (book == null || this.money < book.getPrice() || book.getOwner() != null){
+            return false;
+        } else {
+            this.money -= book.getPrice();
+            book.setOwner(this);
+            this.hisBook = book;
+            return true;
+        }
+    }
+
+    public boolean sellBook(Book book){
+        if (this.hisBook != book || book.getOwner() != this || book == null){
+            return false;
+        } else {
+            this.money += book.getPrice();
+            this.hisBook = null;
+            return true;
+        }
+    }
 }
+
