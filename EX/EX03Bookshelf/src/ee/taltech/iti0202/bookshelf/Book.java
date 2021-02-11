a1b25724dc82c0d0464e9b4fb886498323adda5d
elgedr <elgedr@ttu.ee>
2021-02-11 17:00:54 +0200

    1

--- EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Book.java
+++ EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Book.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Book {
     private static int globalid = -1; //ид должен начинаться с -1. так как при создании делаем +1, как раз первый ид будет 0. эта переменная статическая т.е глобальная
-    private static int futureid = 1;
+    private static int futureid = 0;
 
     private String title;
     private String author;
@@ -27,7 +27,7 @@ public class Book {
         this.price = price;
         globalid += 1; //при создании новой книги у нее будет новый ид. на 1 больше
         this.id = globalid;
-        futureid += 1;
+        futureid = this.id + 1;
     }
 
     /**
