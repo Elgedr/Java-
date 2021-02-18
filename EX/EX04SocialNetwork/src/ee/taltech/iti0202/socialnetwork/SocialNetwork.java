f0da12271f8c3b2be52452477065ef3b75228750
elgedr <elgedr@ttu.ee>
2021-02-18 21:23:52 +0200

    1

--- EX/EX04SocialNetwork/src/ee/taltech/iti0202/socialnetwork/SocialNetwork.java
+++ EX/EX04SocialNetwork/src/ee/taltech/iti0202/socialnetwork/SocialNetwork.java
@@ -1,23 +1,36 @@
 package ee.taltech.iti0202.socialnetwork;
 import ee.taltech.iti0202.socialnetwork.feed.Feed;
 import ee.taltech.iti0202.socialnetwork.group.Group;
+import ee.taltech.iti0202.socialnetwork.message.Message;
 import ee.taltech.iti0202.socialnetwork.user.User;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 
 public class SocialNetwork {
     private Set<Group> registeredGroups;
+    private Feed feed;
 
     public void registerGroup(Group group) {
         registeredGroups.add(group);
     }
 
     public Set<Group> getGroups() {
-        return registeredGroups;
+        return this.registeredGroups;
     }
 
     public Feed getFeedForUser(User user) {
-        return new Feed(null, null);
+        Set<Message> groupMessages = new HashSet<>();
+        for (Group group: this.registeredGroups){
+            for (User userr: group.getParticipants()){
+                if (userr == user){
+                    groupMessages.addAll(group.getMessages());
+                }
+            }
+        }
+        return new Feed(user, groupMessages);
     }
 }
