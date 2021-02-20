49647100d64aebae3ebf8132b4091b4d8ec44c0d
elgedr <elgedr@ttu.ee>
2021-02-20 21:42:15 +0200

    1

--- TK/TK0/src/ee/taltech/iti0202/tk0/cat/Person.java
+++ TK/TK0/src/ee/taltech/iti0202/tk0/cat/Person.java
@@ -1,4 +1,29 @@
 package ee.taltech.iti0202.tk0.cat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Person {
+    private List<Cat> catsList = new ArrayList<>();
+
+    private boolean addCat(Cat cat){
+        if (!catsList.contains(cat)){
+            catsList.add(cat);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Cat> getCats() {
+        return catsList;
+    }
+
+    boolean sellCat(Person sellTo, Cat cat){
+        if (catsList.contains(cat) && !(sellTo == this)){
+            sellTo.addCat(cat);
+            this.catsList.remove(cat);
+            return true;
+        }
+        return false;
+    }
 }
