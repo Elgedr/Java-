451b120eb84f892f6921eafcbf61728a8fb3436f
elgedr <elgedr@ttu.ee>
2021-02-25 18:23:44 +0200

    1

--- TK/TK1/src/ee/taltech/iti0202/tk1/Exam.java
+++ TK/TK1/src/ee/taltech/iti0202/tk1/Exam.java
@@ -67,7 +67,11 @@ public class Exam {
      * mapAB2({"a": "aaa", "b": "bbb", "c": "aaa"}) → {"a": "aaa", "b": "bbb", "c": "aaa"}
      */
     public static Map<String, String> mapAB(Map<String, String> map) {
-        return null;
+        if (map.containsKey("a") && map.containsKey("b") && map.get("a").equals(map.get("b"))){
+            map.remove("a");
+            map.remove("b");
+        }
+        return map;
     }
 
     public static void main(String[] args) {
