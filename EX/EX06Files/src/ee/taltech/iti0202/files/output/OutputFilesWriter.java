1a353f7d4948cafcdb501e71139d9e618298939a
elgedr <elgedr@ttu.ee>
2021-03-06 20:00:17 +0200

    1

--- EX/EX06Files/src/ee/taltech/iti0202/files/output/OutputFilesWriter.java
+++ EX/EX06Files/src/ee/taltech/iti0202/files/output/OutputFilesWriter.java
@@ -3,7 +3,6 @@ package ee.taltech.iti0202.files.output;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
