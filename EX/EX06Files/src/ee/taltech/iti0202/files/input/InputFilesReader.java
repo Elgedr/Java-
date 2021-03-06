df4f8e5dd2a762e4105c14865823eb8c1d07440d
elgedr <elgedr@ttu.ee>
2021-03-06 18:03:40 +0200

    1

--- EX/EX06Files/src/ee/taltech/iti0202/files/input/InputFilesReader.java
+++ EX/EX06Files/src/ee/taltech/iti0202/files/input/InputFilesReader.java
@@ -3,7 +3,7 @@ package ee.taltech.iti0202.files.input;
 import java.util.List;
 
 /**
- * .
+ * Interface.
  */
 public interface InputFilesReader {
     List<String> readTextFromFile(String filename);
--- EX/EX06Files/src/ee/taltech/iti0202/files/morse/MorseTranslator.java
+++ EX/EX06Files/src/ee/taltech/iti0202/files/morse/MorseTranslator.java
@@ -3,7 +3,6 @@ package ee.taltech.iti0202.files.morse;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
